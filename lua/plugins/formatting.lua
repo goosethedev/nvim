9d2ef25376bc4e4ad5ca8dc1ebdf93c284e75a91
Gus Sánchez <goosethedev@proton.me>
2024-05-10 02:18:55 -0400

    Conform.nvim for autoformat

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,13 +1,49 @@
 return {
-  -- Detect tabstop and shiftwidth automatically
-  -- "tpope/vim-sleuth",
+	-- Detect tabstop and shiftwidth automatically
+	-- "tpope/vim-sleuth",
 
-  -- "gc" to comment visual regions/lines
-  { "numToStr/Comment.nvim", opts = {} },
+	-- "gc" to comment visual regions/lines
+	{ "numToStr/Comment.nvim", opts = {} },
 
-  {
-    -- Cmd tools as unified LSP
-    "nvimtools/none-ls.nvim",
-    config = require("configs.none-ls"),
-  },
+	{
+		-- Cmd tools as unified LSP
+		"nvimtools/none-ls.nvim",
+		config = require("configs.none-ls"),
+	},
+
+	{ -- Autoformat
+		"stevearc/conform.nvim",
+		lazy = false,
+		keys = {
+			{
+				"<leader>lf",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "LSP: [F]ormat buffer",
+			},
+		},
+		opts = {
+			notify_on_error = false,
+			format_on_save = function(bufnr)
+				-- Disable "format_on_save lsp_fallback" for languages that don't
+				-- have a well standardized coding style.
+				local disable_filetypes = { c = true, cpp = true }
+				return {
+					timeout_ms = 500,
+					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+				}
+			end,
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform can also run multiple formatters sequentially
+				-- python = { "isort", "black" },
+				--
+				-- You can use a sub-list to tell conform to run *until* a formatter
+				-- is found.
+				-- javascript = { { "prettierd", "prettier" } },
+			},
+		},
+	},
 }
