94518904ab2f1247f42ee06e9b7b58c726a2d94d
Gus Sánchez <goosethedev@proton.me>
2024-05-10 03:00:55 -0400

    nvim-cmp completion w/ luasnip

---
+++ lua/configs/cmp.lua
@@ -0,0 +1,39 @@
+return function()
+	local cmp = require("cmp")
+	-- Load VSCode snippets
+	require("luasnip.loaders.from_vscode").lazy_load()
+
+	cmp.setup({
+		snippet = {
+			-- REQUIRED - snippet engine to use
+			expand = function(args)
+				require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+			end,
+		},
+		-- Floating window settings
+		window = {
+			completion = cmp.config.window.bordered(),
+			documentation = cmp.config.window.bordered(),
+		},
+		-- Mappings for completion when available
+		mapping = cmp.mapping.preset.insert({
+			["<PageUp>"] = cmp.mapping.scroll_docs(-4),
+			["<PageDown>"] = cmp.mapping.scroll_docs(4),
+			["<C-Space>"] = cmp.mapping.complete(),
+			["<Left>"] = cmp.mapping.abort(),
+			["<Esc>"] = cmp.mapping.abort(),
+			["<Right>"] = cmp.mapping.confirm({ select = true }),
+			["<CR>"] = cmp.mapping.confirm({ select = true }),
+		}),
+		-- Sources to import for completions
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" }, -- For luasnip users.
+			{ name = "path" },
+		}, {
+			-- Load this group if none of the above has options
+			-- Info at :h group_index
+			{ name = "buffer" },
+		}),
+	})
+end
--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,56 +1,26 @@
 return {
-	{
-		-- Completions from LSP
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"saadparwaiz1/cmp_luasnip", -- From Luasnip engine
+		"hrsh7th/cmp-nvim-lsp", -- From LSP
+		"hrsh7th/cmp-path", -- From file paths
+		"hrsh7th/cmp-buffer", -- From words in buffer
+
+		{ -- Luasnip snippets engine
+			"L3MON4D3/LuaSnip",
+			dependencies = "rafamadriz/friendly-snippets",
+
+			build = (function()
+				-- Build Step is needed for regex support in snippets.
+				-- This step is not supported in many windows environments.
+				-- Remove the below condition to re-enable on windows.
+				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+					return
+				end
+				return "make install_jsregexp"
+			end)(),
 		},
 	},
-	{
-		"hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-		config = function()
-			local cmp = require("cmp")
-			-- Load VSCode snippets
-			require("luasnip.loaders.from_vscode").lazy_load()
-
-			cmp.setup({
-				snippet = {
-					-- REQUIRED - you must specify a snippet engine
-					expand = function(args)
-						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-						-- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<PageUp>"] = cmp.mapping.scroll_docs(-4),
-					["<PageDown>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<Left>"] = cmp.mapping.abort(),
-					["<Right>"] = cmp.mapping.confirm({ select = true }),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-					-- { name = "vsnip" }, -- For vsnip users.
-					-- { name = 'ultisnips' }, -- For ultisnips users.
-					-- { name = 'snippy' }, -- For snippy users.
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+	config = require("configs.cmp"),
 }
