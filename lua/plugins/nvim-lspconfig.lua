e6cd65c11485d9112563db35be95374eb1e63e37
Gus Sánchez <goosethedev@proton.me>
2024-10-23 10:37:44 -0400

    Comment fixes

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,13 @@
--- List your desired servers with their configs
--- server = {} for default config
+-- server_name = {} for default config
 -- :h lspconfig-all for all preconfigured ones
+
 -- Available keys:
 --  - cmd (table): Override the default command used to start the server
 --  - filetypes (table): Override the default list of associated filetypes for the server
 --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
 --  - settings (table): Override the default settings passed when initializing the server.
+
+-- LSP server configurations
 local servers = {
 	astro = {},
 	cssls = {},
